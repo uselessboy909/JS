1cd413fd14e44cdc991f86b6a26f4823f774ce78
Sergey <markrut23@mail.ru>
2024-10-03 21:29:41 +0400

    удалил всё нахуй

--- test/src/main.ts
+++
@@ -1,9 +0,0 @@
-var strStr = function(haystack : string, needle : string) : number {
-  for(let i = 0; i < haystack.length; i++){
-    if(haystack.substring(i, needle.length) == needle){
-      return i;
-    }
-  }
-  return -1;
-};
-console.log(strStr("sadbutsad", "sad"))
